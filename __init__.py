3a2a069067dbeef5735141da1e8373acf0cc2913
kadams1463 <44598379+kadams1463@users.noreply.github.com>
2018-10-29 23:11:15 -0700

    Initial commit
    Initial commit for the project.

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# Import the libraries from Mycroft.
+from mycroft.skills.core import MycroftSkill
+from adapt.intent import IntentBuilder
+from mycroft.util.log import getLogger
+
+#################################################
+
+# Add the author and enable logging.
+__author__ = 'kadams1463'
+
+LOGGER = getLogger(__name__)
+
+#################################################
+
+# Create the FlipCoinSkill class.
+class FlipCoinSkill(MycroftSkill):
+
+    def __init__(self):
+        super(MyFlipCoinSkill, self).__init__(name="FlipCoinSkill")
+
+    # Create flip_intent using the flip.voc file.
+    def initialize(self):
+        flip_intent = IntentBuilder("FlipIntent").require("flip").build()
+        # Intent callback.
+        self.register_intent(flip_intent, self.handle_flip_intent)
+
+    # Create the dialog from the results.dialog file for Mycroft to speak.
+    def handle_flip_intent(self, message):
+        self.speak_dialog("results")
+
+    def stop(self):
+        pass
+
+def create_skill():
+    return FlipCoinSkill()
